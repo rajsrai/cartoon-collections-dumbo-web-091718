fcfe9c8aeed3fdaa7c5c8411fd59c0596f98f45a
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 17:47:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 
 def long_planeteer_calls(calls)
   calls = ["puff", "go", "two"]
-  calls.collect { |words| calls.length > 4 } ? true : false
+  calls.each { |words| calls.length > 4 } ? true : false
 end
 
 def find_the_cheese# code an argument here
