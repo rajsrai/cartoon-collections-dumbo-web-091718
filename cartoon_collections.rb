5cd958fe27cc941defa2bd15ca547e96976d0225
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 17:36:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,7 @@ end
 end
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls = ["apple", "banana", "orange"]
- planeteer_calls.map! { |calls| calls.capitalize + "!" }
+planeteer_calls.collect { |calls| calls.capitalize + '!'
 end
 
 def long_planeteer_calls# code an argument here
