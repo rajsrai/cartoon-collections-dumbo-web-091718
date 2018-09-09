06239ff74913808de0085daa2f3e20c74c6d58c1
Rajvir Rai <rsr1992@gmail.com>
2018-09-09 17:01:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(arr)
   arr = ['Doc', 'Grumpy', 'Happy', 'Sleepy', 'Bashful', 'Sneezy', 'Dopey']
   arr.each_with_index do |num, idx|
-  end
   puts "#{idx}. #{arr}"
+end
 end
 
 def summon_captain_planet# code an argument here
